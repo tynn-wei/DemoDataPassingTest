9b5feebf1bf17d9181c863032922f3c26f1cc854
TAN TYNN WEI <20019616@myrp.edu.sg>
2021-05-21 00:32:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demodatapassingtest/ThirdActivity.java
@@ -0,0 +1,24 @@
+package com.example.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    TextView tvAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        tvAnswer = findViewById(R.id.textView2);
+        Intent intentReceived = getIntent();
+        char value = intentReceived.getCharExtra("value", 'a');
+        tvAnswer.setText("Character value received is: " + value);
+
+    }
+}
